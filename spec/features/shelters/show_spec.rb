90f8fb7501bba5a2bbf22e0cd91d3f5c4e123579
Alex Gallant <agallant1201@gmail.com>
2020-08-20 15:14:21 -0600

    Add test on shelter show spec for presence and functionality of delete button

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -7,15 +7,31 @@ RSpec.describe 'As a visitor' do
                              city: 'Bangor',
                              state: 'ME',
                              zip: '04401')
+
+     @shop_2 = Shelter.create(name: "Abby's Pals",
+                              address: '456 Second Street',
+                              city: 'Yarmouth',
+                              state: 'ME',
+                              zip: '04102')
+
+    visit "/shelters/#{@shop_1.id}"
   end
 
   it "I see the shelter's information on it's show page" do
-    visit "/shelters/#{@shop_1.id}"
-    
     expect(page).to have_content(@shop_1.name)
     expect(page).to have_content(@shop_1.address)
     expect(page).to have_content(@shop_1.city)
     expect(page).to have_content(@shop_1.state)
     expect(page).to have_content(@shop_1.zip)
+  end
+
+  it "has a link to delete a shelter" do
+    expect(Shelter.all.count).to eq(2)
+
+    click_link 'Delete'
+
+    expect(current_path).to eq('/shelters')
+    expect(Shelter.all.count).to eq(1)
+    expect(page).to_not have_content(@shop_1.name)
   end
 end
