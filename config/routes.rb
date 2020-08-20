ff40d57293dc6a47bdc72537c355edf4782d6000
Alex Gallant <agallant1201@gmail.com>
2020-08-20 15:16:34 -0600

    Add routes for shelter CRUD functionality and pet index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
+  post '/shelters', to: 'shelters#create'
   get '/shelters/:shelter_id', to: 'shelters#show'
+  get '/shelters/:shelter_id/edit', to: 'shelters#edit'
+  patch '/shelters/:shelter_id', to: 'shelters#update'
+  delete '/shelters/:shelter_id', to: 'shelters#destroy'
+
+  get '/pets', to: 'pets#index'
 end
