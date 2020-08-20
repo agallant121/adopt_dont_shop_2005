0fb03434b0deae2a24cb78f86e28cda795b9d4b9
Alex Gallant <agallant1201@gmail.com>
2020-08-20 17:27:53 -0600

    Add description attribute to prevoous tests

---
+++ spec/features/pets/show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  before :each do
+    @shop_1 = Shelter.create(name: "Al's Pals",
+                             address: '123 First Street',
+                             city: 'Bangor',
+                             state: 'ME',
+                             zip: '04401')
+
+    @pet_1 = @shop_1.pets.create(image: 'https://cdn3-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg',
+                                 name: 'Molly',
+                                 age: 3,
+                                 sex: 'F',
+                                 description: 'Nice dog')
+    visit "/pets/#{@pet_1.id}"
+  end
+
+  it "shows a pet's information on the pet's show page" do
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_1.age)
+    expect(page).to have_content(@pet_1.sex)
+    expect(page).to have_content(@pet_1.description)
+    expect(page).to have_content(@pet_1.adopted?)
+  end
+end
