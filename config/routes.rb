8331676bddab58c44e4907bff0a58edd89720693
Alex Gallant <agallant1201@gmail.com>
2020-08-20 17:29:34 -0600

    Add route for pet show page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   delete '/shelters/:shelter_id', to: 'shelters#destroy'
 
   get '/pets', to: 'pets#index'
+  get '/pets/:pet_id', to: 'pets#show'
 
   get '/shelters/:shelter_id/pets', to: 'shelter_pets#index'
 end
