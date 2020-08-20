c7242cda8fea393b828876f68f598a3b60514968
Alex Gallant <agallant1201@gmail.com>
2020-08-20 16:05:47 -0600

    Add shelter_pets index spec to test that a shelter's pets can be seen

---
+++ spec/features/shelter_pets_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  before :each do
+    @shop_1 = Shelter.create(name: "Al's Pals",
+                             address: '123 First Street',
+                             city: 'Bangor',
+                             state: 'ME',
+                             zip: '04401')
+
+    @pet_1 = @shop_1.pets.create(image: 'https://cdn3-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg',
+                                 name: 'Molly',
+                                 age: 3,
+                                 sex: 'F')
+
+    @pet_2 = @shop_1.pets.create(image: 'https://cdn3-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg',
+                                 name: 'Penny',
+                                 age: 3,
+                                 sex: 'F')
+
+    @pet_3 = @shop_1.pets.create(image: 'https://cdn3-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg',
+                                 name: 'Abby',
+                                 age: 3,
+                                 sex: 'F')
+
+    visit "/shelters/#{@shop_1.id}/pets"
+  end
+
+  it "can list the pets that belong to a specific shelter and their information" do
+    # expect(page).to have_content(@pet_1.image)
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_1.age)
+    expect(page).to have_content(@pet_1.sex)
+
+    # expect(page).to have_content(@pet_2.image)
+    expect(page).to have_content(@pet_2.name)
+    expect(page).to have_content(@pet_2.age)
+    expect(page).to have_content(@pet_2.sex)
+
+    # expect(page).to have_content(@pet_3.image)
+    expect(page).to have_content(@pet_3.name)
+    expect(page).to have_content(@pet_3.age)
+    expect(page).to have_content(@pet_3.sex)
+  end
+end
