3d3fd8114c2715c1893db78c7d143515394cf1a6
Alex Gallant <agallant1201@gmail.com>
2020-09-11 15:56:29 -0600

    Add edit, update and destroy actions as well as pet_params

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,26 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:pet_id])
   end
+
+  def edit
+    @pet = Pet.find(params[:pet_id])
+  end
+
+  def update
+    pet = Pet.find(params[:pet_id])
+    pet.update(pet_params)
+    redirect_to "/pets/#{pet.id}"
+  end
+
+  def destroy
+    pet = Pet.find(params[:pet_id])
+    pet.destroy
+    redirect_to '/pets'
+  end
+
+  private
+
+  def pet_params
+    params.permit(:name, :age, :sex, :description)
+  end
 end
