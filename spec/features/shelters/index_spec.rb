a3bfeb7f6f9aa4fa5c007173e4fad8d11386b025
Alex Gallant <agallant1201@gmail.com>
2020-09-11 15:54:13 -0600

    Add test to check for links

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   before :each do
+    Shelter.destroy_all
+    Pet.destroy_all
+    
     @shop_1 = Shelter.create(name: "Al's Pals",
                              address: '123 First Street',
                              city: 'Bangor',
@@ -19,10 +22,10 @@ RSpec.describe 'As a visitor' do
                              zip: '80210')
 
     visit '/shelters'
+    save_and_open_page
   end
 
   it "has a list of the shelter names on shelter index page" do
-
     expect(page).to have_content(@shop_1.name)
     expect(page).to have_content(@shop_2.name)
     expect(page).to have_content(@shop_3.name)
@@ -30,5 +33,11 @@ RSpec.describe 'As a visitor' do
 
   it "has a link to create a new shelter" do
     expect(page).to have_link 'New Shelter'
+  end
+
+  it "has a link next to every shelter to edit that specific shelter" do
+    within "#shelter-#{@shop_1.id}" do
+      expect(page).to have_link 'Edit'
+    end
   end
 end
